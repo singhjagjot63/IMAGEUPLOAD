54de578b5cdedb0ced187c2fffa064d7a6703c26
singhjagjot63 <51919667+singhjagjot63@users.noreply.github.com>
2020-11-03 20:58:27 +0530

    Update upload_image.php

--- upload_image.php
+++ upload_image.php
@@ -1,16 +1,13 @@
 <?php
 
-   $db = mysqli_connect('localhost','root','','JAGJOT');
-   if(!db) {
-   	echo "Database Connection Failed";
+ 
+   
 
-   }
-
-   $image = %_FILES['image'];
+   $image = basename($_FILES['image']['name']);
    $imagePath = 'uploads/'.$image;
    $tmp_name = $_FILES['image']['tmp_name'];
 
    move_uploaded_file($tmp_name, $imagePath);
 
-   $db->query("INSERT INTO images(img)VALUES('".$img."')");
+ 
 ?>
